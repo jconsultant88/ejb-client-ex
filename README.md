f54c7afcaff0943939327036a2bdeac5f725d38b
Venugopal Vemula <jconsultant88@gmail.com>
2015-06-18 16:36:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+1. gfclient.jar which is present in glassfish server is mandatory
+2. Check rmi port if you have any issues with connectivity
---
+++ src/main/java/org/practise/ejb/SampleClientEJB.java
@@ -0,0 +1,37 @@
+package org.practise.ejb;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.practise.ejb.service.CarService;
+
+public class SampleClientEJB {
+
+	public static void main(String[] args) throws Exception {
+		try {
+			accessEJB();
+		} catch (Exception e) {
+			System.err.println("Exception occured " + e.getMessage());
+		}
+	}
+
+	private static void accessEJB() throws NamingException {
+
+		Properties props = new Properties();
+		props.put(Context.INITIAL_CONTEXT_FACTORY,
+				"com.sun.enterprise.naming.SerialInitContextFactory");
+		props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
+
+		props.setProperty("org.omg.CORBA.ORBInitialPort", "51037");
+
+		final InitialContext context = new InitialContext(props);
+
+		CarService carService = (CarService) context
+				.lookup("java:global/ejb-remote-ex-1.0/CarServiceImpl");
+		String str = carService.annualService();
+		System.out.println(str);
+	}
+}
